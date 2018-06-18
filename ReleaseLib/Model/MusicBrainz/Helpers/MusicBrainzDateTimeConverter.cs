5981956908a8a031614f632951aef4ce4e7e6db4
roman412 <romanio412@gmail.com>
2018-06-18 03:34:33 +0300

    MusicBrainzDateTimeConverter overhauled.

--- ReleaseLib/Model/MusicBrainz/Helpers/MusicBrainzDateTimeConverter.cs
+++ ReleaseLib/Model/MusicBrainz/Helpers/MusicBrainzDateTimeConverter.cs
@@ -1,33 +1,50 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Globalization;
 
 namespace ReleaseLib.MusicBrainz
 {
-    public class MusicBrainzDateTimeConverter : JsonConverter
+    /// <summary>
+    /// Custom converter for Json.Net to handle multiple datetime string format.
+    /// </summary>
+    public class MusicBrainzDateTimeConverter : DateTimeConverterBase
     {
-        public static readonly string[] validFormats = new[] { "yyyy", "yyyy-MM", "yyyy-MM-dd" };
-        public override bool CanConvert(Type objectType)
+        public static readonly string[] DefaultInputFormats = new[] { "yyyy", "yyyy-MM", "yyyy-MM-dd" };
+        public static readonly string DefaultOutputFormat = "yyyyMMdd";
+        public static bool DefaultEvaluateEmptyStringAsNull = true;
+
+        private string[] InputFormats = DefaultInputFormats;
+        private string OutputFormat = DefaultOutputFormat;
+        private bool EvaluateEmptyStringAsNull = DefaultEvaluateEmptyStringAsNull;
+
+        public MusicBrainzDateTimeConverter()
         {
-            return (objectType == typeof(DateTime));
+        }
+
+        public MusicBrainzDateTimeConverter(string[] inputFormats, string outputFormat, bool evaluateEmptyStringAsNull = true)
+        {
+            if (inputFormats != null) InputFormats = inputFormats;
+            if (outputFormat != null) OutputFormat = outputFormat;
+            EvaluateEmptyStringAsNull = evaluateEmptyStringAsNull;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             DateTime result;
             string dateString = JToken.Load(reader).ToObject<string>();
-            var success = DateTime.TryParseExact(dateString, validFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+
+            var success = DateTime.TryParseExact(dateString, InputFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
             if (success != true)
-            {
                 return null;
-            }
+
             return result;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            writer.WriteValue(((DateTime)value).ToString(OutputFormat));
         }
     }
 }
